436a2a14d995deb533ab9ebe327fb621601d53a6
keyboardDrummer <rgv.willems@gmail.com>
2016-05-29 10:37:57 +0200

    Added some Rx example.

--- SmartReactives.Examples/Examples.cs
+++ SmartReactives.Examples/Examples.cs
@@ -6,55 +6,66 @@ using SmartReactives.Extensions;
 
 namespace SmartReactives.Examples
 {
-	class Examples
+    class Examples
     {
-
         [Test]
         public void Square()
         {
             var input = new ReactiveVariable<int>(1);
             var inputSquared = new ReactiveExpression<int>(() => input.Value * input.Value);
-            inputSquared.Subscribe(getSquare => Console.WriteLine("square = " + getSquare()));
+            inputSquared.Subscribe(getSquare => Console.WriteLine("square = " + getSquare())); // Prints 'square = 1'
 
-            input.Value = 2;
-            input.Value = 3;
+            input.Value = 2; // Prints 'square = 4'
+            input.Value = 3; // Prints 'square = 9'
         }
 
         [Test]
         public void RxSquare()
         {
-            var inputs = new Subject<int>();
+            var inputs = new BehaviorSubject<int>(1);
             var inputSquared = inputs.Select(input => input * input);
-            inputSquared.Subscribe(square => Console.WriteLine("square = " + square));
+            inputSquared.Subscribe(square => Console.WriteLine("square = " + square)); // Prints 'square = 1'
 
-            inputs.OnNext(2);
-            inputs.OnNext(3);
+            inputs.OnNext(2); // Prints 'square = 4'
+            inputs.OnNext(3); // Prints 'square = 9'
         }
 
         [Test]
-	    public void Nesting()
-	    {
-	        var input = new ReactiveVariable<int>();
+        public void Composition()
+        {
+            var input = new ReactiveVariable<int>();
             var timesTwo = new ReactiveExpression<int>(() => input.Value * 2);
             var timesThree = new ReactiveExpression<int>(() => input.Value * 3);
             var sumOfBoth = new ReactiveExpression<int>(() => timesTwo.Evaluate() + timesThree.Evaluate());
-	        sumOfBoth.Subscribe(getValue => Console.WriteLine("sumOfBoth = " + getValue())); //Prints 'sumOfBoth = 0'
+            sumOfBoth.Subscribe(getValue => Console.WriteLine("sumOfBoth = " + getValue())); //Prints 'sumOfBoth = 0'
             input.Value = 1; //Prints 'sumOfBoth = 5'
             input.Value = 2; //Prints 'sumOfBoth = 10'
         }
 
-		[Test]
-		public void Precise()
-		{
-			var left = new ReactiveVariable<bool>();
-			var right = new ReactiveVariable<bool>();
-			var leftOrRight = new ReactiveExpression<bool>(() => left.Value || right.Value);
-			leftOrRight.Subscribe(getValue => Console.WriteLine("leftOrRight = " + getValue())); // Prints 'leftOrRight = false'
+        [Test]
+        public void RxComposition()
+        {
+            var input = new BehaviorSubject<int>(0);
+            var timesTwo = input.Select(value => value * 2);
+            var timesThree = input.Select(value => value * 3);
+            var sumOfBoth = timesTwo.CombineLatest(timesThree, (first, second) => first + second);
+            sumOfBoth.Subscribe(value => Console.WriteLine("sumOfBoth = " + value)); //Prints 'sumOfBoth = 0'
+            input.OnNext(1); //Prints 'sumOfBoth = 2' and 'sumOfBoth = 5'
+            input.OnNext(2); //Prints 'sumOfBoth = 7' and 'sumOfBoth = 10'
+        }
 
-			right.Value = true; // Prints 'leftOrRight = true'
-			left.Value = true; // Prints 'leftOrRight = true'
-			right.Value = false; // Prints nothing
-		}
+        [Test]
+        public void Precise()
+        {
+            var left = new ReactiveVariable<bool>();
+            var right = new ReactiveVariable<bool>();
+            var leftOrRight = new ReactiveExpression<bool>(() => left.Value || right.Value);
+            leftOrRight.Subscribe(getValue => Console.WriteLine("leftOrRight = " + getValue())); // Prints 'leftOrRight = False'
+
+            right.Value = true; // Prints 'leftOrRight = True'
+            left.Value = true; // Prints 'leftOrRight = True'
+            right.Value = false; // Prints nothing
+        }
 
         [Test]
         public void RxPrecise()
@@ -62,30 +73,30 @@ namespace SmartReactives.Examples
             var lefts = new BehaviorSubject<bool>(false);
             var rights = new BehaviorSubject<bool>(false);
             var leftOrRight = lefts.CombineLatest(rights, (left, right) => left || right);
-                       
-            leftOrRight.Subscribe(value => Console.WriteLine("leftOrRight = " + value)); // Prints 'leftOrRight = false'
 
-            rights.OnNext(true); // Prints 'leftOrRight = true'
-            lefts.OnNext(true); // Prints 'leftOrRight = true'
-            rights.OnNext(false); // Prints nothing
+            leftOrRight.Subscribe(value => Console.WriteLine("leftOrRight = " + value)); // Prints 'leftOrRight = False'
+
+            rights.OnNext(true); // Prints 'leftOrRight = True'
+            lefts.OnNext(true); // Prints 'leftOrRight = True'
+            rights.OnNext(false); // Prints 'leftOrRight = True' (while SmartReactives prints nothing here)
         }
 
         [Test]
-		public void Cache()
-		{
-			var input = new ReactiveVariable<int>(2); //We define a reactive variable.
-			Func<int> f = () => //f is the calculation we want to cache.
-			{
-				Console.WriteLine("f was evaluated");
-				return input.Value * input.Value; //f depends on our reactive variable input.
-			};
-			var cache = new ReactiveCache<int>(f); //We base our cache on f.
+        public void Cache()
+        {
+            var input = new ReactiveVariable<int>(2); //We define a reactive variable.
+            Func<int> f = () => //f is the calculation we want to cache.
+            {
+                Console.WriteLine("f was evaluated");
+                return input.Value * input.Value; //f depends on our reactive variable input.
+            };
+            var cache = new ReactiveCache<int>(f); //We base our cache on f.
 
-			Console.WriteLine("f() = " + cache.Get()); //Cache was not set so we evaluate f.
-			Console.WriteLine("f() = " + cache.Get()); //Cache is set so we don't evaluate f.
+            Console.WriteLine("f() = " + cache.Get()); //Cache was not set so we evaluate f.
+            Console.WriteLine("f() = " + cache.Get()); //Cache is set so we don't evaluate f.
 
-			input.Value = 3; //We change our input variable, causing our cache to become stale.
-			Console.WriteLine("f() = " + cache.Get()); //Cache is stale, so we must evaluate f.
-		}
-	}
+            input.Value = 3; //We change our input variable, causing our cache to become stale.
+            Console.WriteLine("f() = " + cache.Get()); //Cache is stale, so we must evaluate f.
+        }
+    }
 }
