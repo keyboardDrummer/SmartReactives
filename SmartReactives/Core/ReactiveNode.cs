c90e743e36c631d9655189c13cc125b8e3df82f3
keyboardDrummer <rgv.willems@gmail.com>
2016-05-30 18:14:06 +0200

    updated comment

--- SmartReactives/Core/ReactiveNode.cs
+++ SmartReactives/Core/ReactiveNode.cs
@@ -6,6 +6,22 @@ namespace SmartReactives.Core
 {
 	/// <summary>
     /// A node in the dependency graph of <see cref="ReactiveManager"/>
+    /// We define forward in the dependency graph as moving from dependencies to dependents.
+    /// So given that a node is changed, we can find all effected dependents by traversing forward through the graph from the changed node.
+    /// After notifying a node, we must delete all incoming edges of this node to prevent false notifications.
+    /// 
+    /// The graph is designed with speed in mind. These properties are important to consider:
+    /// 1) We only traverse forwards.
+    /// 2) Every edge is only traversed once.
+    /// 3) We often add an incoming edge.
+    /// 4) We often want to delete all incoming edges of a node.
+    /// 
+    /// Because we often add outgoing edges but only traverse them once, we store a linked list of successors for each node.
+    /// The linked list is an appropriate situation for a write often read once situation.
+    /// 
+    /// We store a sort of timestamp in the field notificationsHad both on the nodes and on the edges in the graph.
+    /// While traversing the graph, we skip edges whose timestamp does not match of their target node, as if these edges were not part of the graph. 
+    /// Incrementing the timestamp of a node is thus a cheap way to effectively delete all incoming edges of this node. 
     /// </summary>
 	class ReactiveNode
 	{
