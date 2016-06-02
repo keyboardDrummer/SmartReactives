ed9991b3df4ff39f7c87f28f98fcf17afafff4d4
keyboardDrummer <rgv.willems@gmail.com>
2016-06-02 23:36:31 +0200

    Updated package number.

--- SmartReactives/Properties/AssemblyInfo.cs
+++ SmartReactives/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyVersion("2.3.0")]
